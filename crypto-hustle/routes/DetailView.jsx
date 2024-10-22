d8753a57b15c4e3b72d9de1b1d6cf906bd38b176
Brandon Gonzalez <120545284+BrandonnGonzalez@users.noreply.github.com>
2024-10-22 01:04:41 -0400

    Create DetailView.jsx

---
+++ crypto-hustle/routes/DetailView.jsx
@@ -0,0 +1,12 @@
+import CoinDetail from '../Components/CoinDetail';
+
+const DetailView = () => {
+  return (
+    <div>
+      <h2>Coin Details</h2>
+      <CoinDetail />
+    </div>
+  );
+};
+
+export default DetailView;
