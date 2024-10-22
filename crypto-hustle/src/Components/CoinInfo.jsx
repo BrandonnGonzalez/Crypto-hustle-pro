6fa14cf8d13ede69ff896a610ca136c5de476db8
Brandon Gonzalez <120545284+BrandonnGonzalez@users.noreply.github.com>
2024-10-22 01:05:40 -0400

    Update CoinInfo.jsx

--- crypto-hustle/src/Components/CoinInfo.jsx
+++ crypto-hustle/src/Components/CoinInfo.jsx
@@ -1,21 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
-const CoinInfo = ({ image, name, symbol }) => {
-  const [price, setPrice] = useState(null);
-
-  useEffect(() => {
-    const getCoinPrice = async () => {
-      const response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=e3c99471144d34c8d67d7e96d88d9a2ad548761df97c7f6b138a584b0e55fcae`);
-      const data = await response.json();
-      setPrice(data.USD);
-    };
-    getCoinPrice();
-  }, [symbol]);
-
+const CoinInfo = ({ name, symbol, image }) => {
   return (
     <li className="coin-info">
-      <img src={`https://www.cryptocompare.com${image}`} alt={`Icon of ${name}`} />
-      <span>{name} ({symbol}) - ${price} USD</span>
+      <img src={`https://www.cryptocompare.com${image}`} alt={name} />
+      <Link to={`/coinDetails/${symbol}`} style={{ color: 'white' }}>
+        {name} <span className="tab"></span>
+      </Link>
     </li>
   );
 };
